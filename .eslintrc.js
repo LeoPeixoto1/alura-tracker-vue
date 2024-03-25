02878616a918e0e210cc205646b9f56bc8496643
Leonardo Peixoto <lpeixotom@gmail.com>
2024-03-25 18:05:00 -0300

    1.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,9 @@ module.exports = {
     ecmaVersion: 2020
   },
   rules: {
+    'vue/multi-word-component-names': 0,
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
 }
+
---
+++ src/components/Tarefa.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="lista">
+        <Box>
+            <div class="columns">
+                <div class="column is-7">
+                    {{tarefa.descricao || 'Tarefa sem descrição' }}
+                </div>
+                <div class="column">
+                    <CronometroTask :tempoEmSegundos="tarefa.duracaoEmSegundos"/>
+                </div>
+            </div>
+        </Box>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import CronometroTask from './CronometroTask.vue'
+import ITarefa from '@/interfaces/ITarefa';
+import Box from './Box.vue'
+
+
+
+export default defineComponent({
+    name: 'Tarefa',
+    components: {
+        CronometroTask,
+        Box
+    },
+    props: {
+        tarefa: {
+            type: Object as PropType<ITarefa>,
+                required: true
+        }
+    }
+})
+</script>
